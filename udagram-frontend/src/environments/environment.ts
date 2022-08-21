9dbb125b0af56a8c0cc452467c0fba2c40f4153d
Abdullatif Habiba <abdo9866abdo@gmail.com>
2022-08-21 22:34:48 +0200

    upload-images-for-submit

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ac02ece2cf5894d8884ae015f0da3b7a-746277176.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
